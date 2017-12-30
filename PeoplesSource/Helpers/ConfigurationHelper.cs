7b692d6c1bd1dce38b1af6ef054584d7fcafdf45
emeraldjecko <emeraldjecko@yahoo.com>
2017-12-30 08:16:30 -0800

    Added missing helper class

---
+++ PeoplesSource/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace PeoplesSource.Helpers
+{
+    public class ConfigurationHelper
+    {
+        public static string GetTeapplixDownloadFolderPath()
+        {
+            return ConfigurationManager.AppSettings["TeapplixDownloadFolderPath"].ToString();
+        }
+        public static string GetTeapplixProductURL()
+        {
+            return ConfigurationManager.AppSettings["TeapplixProductURL"].ToString();
+        }
+        public static string GetTeapplixQuantityURL()
+        {
+            return ConfigurationManager.AppSettings["TeapplixQuantityURL"].ToString();
+        }
+        public static string GetTeapplixOrderURL()
+        {
+            return ConfigurationManager.AppSettings["TeapplixOrderURL"].ToString();
+        }
+        public static string GetSSISProductPath()
+        {
+            return ConfigurationManager.AppSettings["SSISProductPath"].ToString();
+        }
+        public static string GetSSISQuantityPath()
+        {
+            return ConfigurationManager.AppSettings["SSISQuantityPath"].ToString();
+        }
+        public static string GetSSISOrderPath()
+        {
+            return ConfigurationManager.AppSettings["SSISOrderPath"].ToString();
+        }
+    }
+}
